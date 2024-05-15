3cc5148ce7c0b365e6a208a739f26b8d913f6685
carlos <carlos.s2006@aluno.ifsc.edu.br>
2024-05-15 17:43:44 -0300

    DAO completo

--- estudoDeCaso/src/Control/ProdutoDAO.java
+++ estudoDeCaso/src/Control/ProdutoDAO.java
@@ -1,6 +1,8 @@
 package Control;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
 
 import model.Produto;
 
@@ -8,8 +10,19 @@ public class ProdutoDAO {
 	
 	private ArrayList<Produto> estoque;
 	private int geradorId;
+	private static ProdutoDAO instancia;
+	Scanner leitor = new Scanner(System.in);
+
+	public static ProdutoDAO getInstancia() {
+		if(instancia == null) {
+			instancia = new ProdutoDAO();
+		}
+		
+		return instancia;
+	}
 	
-	public ProdutoDAO() {
+	
+	private ProdutoDAO() {
 		this.estoque = new ArrayList<>();
 		this.geradorId = 0;
 	}
@@ -19,12 +32,54 @@ public class ProdutoDAO {
 		produto.setId(geradorId);
 			geradorId++;
 			estoque.add(produto);
-		
+			
+			System.out.println("Adicione os valor do produto"); 
+			
+			System.out.println("data de chegada"); 
+			String DataC = leitor.nextLine(); 
+			
+			System.out.println("Data de validade");
+			String DataValidade = leitor.nextLine();
+			
+			System.out.println("Fabricante");
+			String Fabricante = leitor.nextLine(); 
+			
+			System.out.println("Nome:");
+			String Nome = leitor.nextLine(); 
+			 
+			System.out.println("Preço mercado");
+			String a = leitor.nextLine(); 
+			Float PrecoMercado = Float.valueOf(a); 
+			 System.out.println("qualidade");
+			String Qualidade = leitor.nextLine(); 
+			 System.out.println("quantidade");
+			String B = leitor.nextLine(); 
+			 Integer Quantidade = Integer.valueOf(B); 
+
 		
 		return produto.getId();
 	}
 	
 	public ArrayList<Produto> listar(){
 		return this.estoque;
+	}
+	public boolean remover(int id) {
+	    for (Iterator<Produto> iterator = estoque.iterator(); iterator.hasNext();) {
+	        Produto produto = iterator.next();
+	        if (produto.getId() == id) {
+	            iterator.remove();
+	            return true;
+	        }
+	    }
+	    return false;
+	}
+	
+	public Produto buscarPorId(int id) {
+	    for (Produto produto : estoque) {
+	        if (produto.getId() == id) {
+	            return produto;
+	        }
+	    }
+	    return null;
 	}
 }
